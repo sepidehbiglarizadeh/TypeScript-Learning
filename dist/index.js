53b6680d322d7d68fdb8ea8175a63599e573f1b7
sepideh-biglarizadeh <sepideh.biglarizadeh00@gmail.com>
2023-01-21 20:11:01 +0330

    Types , Array , Tuple , Enum , Function, Object

--- dist/index.js
+++ dist/index.js
@@ -3,4 +3,30 @@ let age = 31;
 if (age < 50)
     age += 10;
 console.log(age);
+let sales = 123456789;
+let course = "TypeScript";
+let isPublished = true;
+let level;
+let numbers = [1, 2, 3];
+let user = [1, "sepideh"];
+var Size;
+(function (Size) {
+    Size["Small"] = "s";
+    Size["Medium"] = "m";
+    Size["Large"] = "l";
+})(Size || (Size = {}));
+let mySize = Size.Small;
+function calculateTax(income, taxYear = 2022) {
+    if (taxYear < 50000)
+        return income * 1.2;
+    return income * 1.3;
+}
+calculateTax(10000);
+let employee = {
+    id: 1,
+    name: "sepideh",
+    retire: (date) => {
+        console.log(date);
+    },
+};
 //# sourceMappingURL=index.js.map
