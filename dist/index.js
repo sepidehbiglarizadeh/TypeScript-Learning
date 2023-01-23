2327b527c7fb7eadd7d1701bd6b0b36f08e161b0
sepideh-biglarizadeh <sepideh.biglarizadeh00@gmail.com>
2023-01-23 21:01:08 +0330

    Type Aliases

--- dist/index.js
+++ dist/index.js
@@ -1,27 +1,4 @@
 "use strict";
-let age = 31;
-if (age < 50)
-    age += 10;
-console.log(age);
-let sales = 123456789;
-let course = "TypeScript";
-let isPublished = true;
-let level;
-let numbers = [1, 2, 3];
-let user = [1, "sepideh"];
-var Size;
-(function (Size) {
-    Size["Small"] = "s";
-    Size["Medium"] = "m";
-    Size["Large"] = "l";
-})(Size || (Size = {}));
-let mySize = Size.Small;
-function calculateTax(income, taxYear = 2022) {
-    if (taxYear < 50000)
-        return income * 1.2;
-    return income * 1.3;
-}
-calculateTax(10000);
 let employee = {
     id: 1,
     name: "sepideh",
@@ -29,4 +6,5 @@ let employee = {
         console.log(date);
     },
 };
+console.log(employee);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,46 +1,16 @@
-let age: number = 31;
-
-if (age < 50) age += 10;
-
-console.log(age);
-
-// Types
-let sales = 123_456_789;
-let course = "TypeScript";
-let isPublished = true;
-let level;
-
-// Array
-let numbers: number[] = [1, 2, 3];
-
-// Tuple
-let user: [number, string] = [1, "sepideh"];
-
-// Enum
-enum Size {
-  Small = "s",
-  Medium = "m",
-  Large = "l",
-}
-
-let mySize: Size = Size.Small;
-
-// Function
-function calculateTax(income: number, taxYear = 2022): number {
-  if (taxYear < 50_000) return income * 1.2;
-  return income * 1.3;
-}
-calculateTax(10_000);
-
-// Object
-let employee: {
+type Employee={
   readonly id: number;
   name: string;
   retire: (date: Date) => void;
-} = {
+}
+
+let employee: Employee = {
   id: 1,
   name: "sepideh",
   retire: (date: Date) => {
     console.log(date);
   },
 };
+
+console.log(employee);
+
