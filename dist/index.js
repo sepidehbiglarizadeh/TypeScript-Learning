33e514328631e3395bf6fdbad180eda9ccaf6159
sepideh-biglarizadeh <sepideh.biglarizadeh00@gmail.com>
2023-01-24 20:54:32 +0330

    dynamic property

--- dist/index.js
+++ dist/index.js
@@ -1,25 +1,8 @@
 "use strict";
-class Account {
-    constructor(id, name, _balance) {
-        this.id = id;
-        this.name = name;
-        this._balance = _balance;
-    }
-    deposite(amount) {
-        if (amount <= 0)
-            throw new Error("Invalid amount");
-        this._balance += amount;
-    }
-    get balance() {
-        return this._balance;
-    }
-    set balance(value) {
-        if (value < 0)
-            throw new Error("invalid value");
-        this._balance = value;
-    }
+class SeatAssignment {
 }
-let account = new Account(1, "sepideh", 0);
-account.balance = 1;
-console.log(account.balance);
+let seats = new SeatAssignment();
+seats.A1 = "sepideh";
+seats.A2 = "shaghayegh";
+console.log(seats);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,9 @@
-class Account {
-  nickname?: string;
-
-  constructor(
-    public readonly id: number,
-    public name: string,
-    private _balance: number
-  ) {}
-
-  deposite(amount: number): void {
-    if (amount <= 0) throw new Error("Invalid amount");
-    this._balance += amount;
-  }
-
-  get balance(): number {
-    return this._balance;
-  }
-
-  set balance(value: number) {
-    if (value < 0) throw new Error("invalid value");
-    this._balance = value;
-  }
+class SeatAssignment {
+  [seatNumber: string]: string;
 }
 
-let account = new Account(1, "sepideh", 0);
-// account.deposite(100);
-account.balance = 1;
-console.log(account.balance);
+let seats = new SeatAssignment();
+seats.A1 = "sepideh";
+seats.A2 = "shaghayegh";
+
+console.log(seats);
