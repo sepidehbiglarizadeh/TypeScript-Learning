678dc6fa6df5abed698f0e35b38217125c0f8ba3
sepideh-biglarizadeh <sepideh.biglarizadeh00@gmail.com>
2023-01-24 20:29:29 +0330

    class in typescript

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,20 @@
-function processEvents():never {
-  while (true) {
-    
+class Account {
+  id: number;
+  name: string;
+  balance: number;
+
+  constructor(id: number, name: string, balance: number) {
+    this.id = id;
+    this.name = name;
+    this.balance = balance;
+  }
+
+  deposite(amount: number): void {
+    if (amount <= 0) throw new Error("Invalid amount");
+    this.balance += amount;
   }
 }
 
-processEvents();
-console.log('hey');
+let account = new Account(1, "sepideh", 0);
+account.deposite(100);
+console.log(account.balance);
