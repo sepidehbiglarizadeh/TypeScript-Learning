082c24c628fc0322d90216ac5e8658b7340c1ad4
sepideh-biglarizadeh <sepideh.biglarizadeh00@gmail.com>
2023-01-25 19:51:42 +0330

    Type mapping

--- src/index.ts
+++ src/index.ts
@@ -3,19 +3,11 @@ interface Product {
   price: number;
 }
 
-class Store<T> {
-  protected _objects: T[] = [];
+type ReadonlyProduct = {
+  readonly [k in keyof Product]: Product[k];
+};
 
-  add(obj: T): void {
-    this._objects.push(obj);
-  }
-
-  find(property: keyof T, value: unknown): T | undefined {
-    return this._objects.find((obj) => obj[property] === value);
-  }
-}
-
-let store = new Store<Product>();
-store.add({ name: "a", price: 1 });
-store.find("name", "a");
-store.find("price", 1);
+let product: ReadonlyProduct = {
+  name: "a",
+  price: 1,
+};
