95cd314950e8499c2d888b6d5d250fbc2d98a909
sepideh-biglarizadeh <sepideh.biglarizadeh00@gmail.com>
2023-01-27 17:28:34 +0330

    Method Decorator

--- dist/index.js
+++ dist/index.js
@@ -5,24 +5,22 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-function Component(options) {
-    return (constructor) => {
-        console.log("component decorator called");
-        constructor.prototype.options = options;
-        constructor.prototype.uniqueId = Date.now();
-        constructor.prototype.insertDOM = () => {
-            console.log("inserting the component in the DOM");
-        };
+function Log(target, methodName, descriptor) {
+    const original = descriptor.value;
+    descriptor.value = function (...args) {
+        console.log("Before");
+        original.call(this, ...args);
+        console.log("After");
     };
 }
-function Pipe(constructor) {
-    console.log("pipe decorator called");
-    constructor.prototype.pipe = true;
+class Person {
+    say(message) {
+        console.log("person says " + message);
+    }
 }
-let ProfileComponent = class ProfileComponent {
-};
-ProfileComponent = __decorate([
-    Pipe,
-    Component({ selector: "#profile-img" })
-], ProfileComponent);
+__decorate([
+    Log
+], Person.prototype, "say", null);
+let person = new Person();
+person.say("Hello");
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,18 @@
-type ComponentOptions = {
-  selector: string;
-};
-
-function Component(options: ComponentOptions) {
-  return (constructor: Function) => {
-    console.log("component decorator called");
-    constructor.prototype.options = options;
-    constructor.prototype.uniqueId = Date.now();
-    constructor.prototype.insertDOM = () => {
-      console.log("inserting the component in the DOM");
-    };
+function Log(target: any, methodName: string, descriptor: PropertyDescriptor) {
+  const original = descriptor.value as Function;
+  descriptor.value = function (...args: any) {
+    console.log("Before");
+    original.call(this, ...args);
+    console.log("After");
   };
 }
 
-function Pipe(constructor: Function) {
-  console.log("pipe decorator called");
-  constructor.prototype.pipe = true;
+class Person {
+  @Log
+  say(message: string) {
+    console.log("person says " + message);
+  }
 }
 
-@Pipe
-@Component({ selector: "#profile-img" })
-class ProfileComponent {}
+let person = new Person();
+person.say("Hello");
